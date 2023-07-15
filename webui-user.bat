6814999c7df966240c7fee12b097fed202c48ed8
KINGJMoney <127001267+KINGJMoney@users.noreply.github.com>
2023-07-15 17:27:22 -0500

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,7 +1,22 @@
+
 @echo off
 
-set PYTHON=
-set GIT=
+set PYTHON=python.defaultInterpreterPath": "C:\\Users\\psych\\AppData\\Local\\Microsoft\\WindowsApps\\python3.11.exe"
+set GIT=git.defaultInterpreterPath": "C:\\Program Files\\Git\\bin\\git.exe"
+set VENV_DIR=venv.defaultInterpreterPath": "C:\\Users\\psych\\Documents\\Python\\PythonProjects\\webui\\venv"
+set COMMANDLINE_ARGS=commandLineArgs": ["-m", "flask", "run"]
+
+call webui.bat
+
+REM Path: webui-dev.bat
+
+@echo off
+
+set PYTHON=python.defaultInterpreterPath": "C:\\Users\\psych\\AppData\\Local\\Microsoft\\WindowsApps\\python3.11.exe"
+set GIT=git.defaultInterpreterPath": "C:\\Program Files\\Git\\bin\\git.exe"
+set VENV_DIR=venv.defaultInterpreterPath": "C:\\Users\\psych\\Documents\\Python\\PythonProjects\\webui\\venv"
+set COMMANDLINE_ARGS=commandLineArgs": ["-m", "flask", "run", "--host=
+
 set VENV_DIR=
 set COMMANDLINE_ARGS=
 
